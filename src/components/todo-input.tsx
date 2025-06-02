9a32a73a47c2bc8e00d509f3fa6982a040a6d135
jaketysome <jaketysome@gmail.com>
2025-06-02 14:41:11 +0100

    moves newtask and input handlers to todo-input component

--- src/components/todo-input.tsx
+++ src/components/todo-input.tsx
@@ -1,16 +1,31 @@
+import { useState, type Dispatch, type SetStateAction } from "react";
 import type { Task, TodoListStatus } from "../types";
 
 function TodoInput({
   status,
-  newTask,
-  handleInputChange,
-  handleNewTask,
+  setStatus,
+  tasks,
+  setTasks,
 }: {
   status: TodoListStatus;
-  newTask: Task;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleNewTask: () => void;
+  setStatus: Dispatch<SetStateAction<TodoListStatus>>;
+  tasks: Task[];
+  setTasks: Dispatch<SetStateAction<Task[]>>;
 }) {
+  const [newTask, setNewTask] = useState<Task>({ text: "", isComplete: false });
+
+  const handleNewTask = () => {
+    setTasks([...tasks, newTask]);
+    setNewTask({ ...newTask, text: "" });
+    setStatus("success");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setStatus("typing");
+    setNewTask({ ...newTask, text: e.target.value });
+  };
+
   return (
     <div className='todo-input'>
       <input
--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -16,20 +16,7 @@ const initialState = [
 
 export default function TodoList() {
   const [status, setStatus] = useState<TodoListStatus>("empty");
-  const [newTask, setNewTask] = useState<Task>({ text: "", isComplete: false });
   const [tasks, setTasks] = useState<Task[]>(initialState);
-
-  const handleNewTask = () => {
-    setTasks([...tasks, newTask]);
-    setNewTask({ ...newTask, text: "" });
-    setStatus("success");
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setStatus("typing");
-    setNewTask({ ...newTask, text: e.target.value });
-  };
 
   return (
     <div className='todo-list'>
@@ -38,7 +25,7 @@ export default function TodoList() {
           <TodoItem key={index} {...{ task, index, tasks, setTasks }} />
         ))}
       </ul>
-      <TodoInput {...{ status, newTask, handleInputChange, handleNewTask }} />
+      <TodoInput {...{ status, setStatus, tasks, setTasks }} />
     </div>
   );
 }
